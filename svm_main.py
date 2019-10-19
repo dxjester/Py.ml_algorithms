5af554881f8638ff7e104d131015ed496d80b8d3
dxjester <patbenitez@gmail.com>
2019-10-19 17:08:18 -0400

    added csv and excel import funciton templates

--- svm_main.py
+++ svm_main.py
@@ -3,3 +3,18 @@
 # DATE CREATED: 19-OCT-19
 # DATE UPDATED: 19-OCT-19
 # VERSION: 1.0
+
+import pandas as pd
+import numpy as np
+import scikit as sc
+
+def csv_import():
+    '''
+        A function designed to import a .csv file
+    '''
+    
+def excel_import():
+    '''
+        A function designed to import an .xlsx file
+    '''
+    
